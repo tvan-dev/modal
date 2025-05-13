abc0bd5debc53eda3daca0a48222cf30036c0abd
Tvan <hothithanhvan.dev@gmail.com>
2025-05-13 21:28:23 +0700

    3. Modal contructor

--- main.js
+++ main.js
@@ -2,61 +2,67 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 
-function showModal(content) {
-    const backdrop = document.createElement('div');
-    backdrop.className = "modal-backdrop";
-    backdrop.innerHTML = `
-        <div class="modal-container">
-            <button class="modal-close">&times;</button>
-            <div class="modal-content">
-                ${content}
-            </div>
-        </div>
-    `
-    document.body.appendChild(backdrop);
-    // hàm setTimeout này sẽ delay 0ms để cho css transition hoạt động
-    // nếu không có hàm này thì css transition sẽ không hoạt động
-    setTimeout(function() {
-        backdrop.classList.add('show');
-    },0)
+function Modal () {
+    this.openModal = function (content) {
+        const backdrop = document.createElement("div");
+        backdrop.className = "modal-backdrop";
 
-    // Bắt sự kiện click vào nút close để đóng modal
-    backdrop.querySelector(".modal-close").onclick = function() {
-        closeModal(backdrop);
-    }
+        const container = document.createElement("div");
+        container.className = "modal-container";
 
-    // Bắt sự kiện click vào backdrop để đóng modal
-    backdrop.onclick = function(e) {
-        if(e.target === this) {
-            closeModal(backdrop);
+        const btnClose = document.createElement("button");
+        btnClose.className = "modal-close";
+        btnClose.innerHTML = "&times;"
+
+        const contents = document.createElement("div");
+        contents.className = "modal-content";
+        
+        contents.innerHTML = content;
+        container.append(btnClose, contents);
+        backdrop.append(container);
+        document.body.append(backdrop);
+        setTimeout(function() {
+            backdrop.classList.add("show");
+        },0)
+
+        //Attach event listeners
+        btnClose.onclick = () => {
+            this.closeModal(backdrop);
+            
+        }
+
+        backdrop.onclick =  (e) =>{
+            if (e.target === backdrop) {
+                this.closeModal(backdrop);
+            }
+        }
+
+        document.addEventListener("keydown",  (e) => {
+            if (e.key === "Escape") {
+                this.closeModal(backdrop);
+            }
+        })
+    };
+    this.closeModal = function(element) {
+        element.classList.remove("show");
+        element.ontransitionend = function () {
+            element.remove(); 
         }
     }
-    // Bắt sự kiện ESC để đóng modal
-    document.onkeydown = function(e) {
-        if(e.key === "Escape") {
-            closeModal(backdrop);
-        }   
-    }
 }
 
+const modal = new Modal();
+// const template1 = $(".modal-1").innerHTML;
+const template2 = $(".modal-2").innerHTML;
 
-// Bắt sự kiện click vào từng nút để mở modal, truyền nội dung trực tiếp vào hàm showModal
 
-$(".btn-1").onclick = function() {
-    showModal("<h1>Modal 1  : The order property specifies the order of the flex items inside the flex container.</h1>");
-}
-$(".btn-2").onclick = function() {
-    showModal("<h3>Modal 2  : The order propertr.</h3>");
-}
-$(".btn-3").onclick = function() {
-    showModal("<p>Modal 3  :rope order of the flex items inside the flex container.</p>");
-}
 
-function closeModal(element) {
-    element.classList.remove('show');
-    
-    // Đợi hiệu ứng kết thúc mới remove
-    element.addEventListener('transitionend', () => {
-        element.remove();
-    }, { once: true }); // `once: true` giúp lắng nghe 1 lần duy nhất
+$("#open-modal-1").onclick = function () {
+    modal.openModal(`${$(".modal-1").innerHTML}`);
+}
+$("#open-modal-2").onclick = function () {
+    modal.openModal(`${template2}`);
+}
+$("#open-modal-3").onclick = function () {
+    modal.openModal("<h1>Modal 3: content akfnf container.</h1>")
 }
