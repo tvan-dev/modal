4cf9ef807636a04169b1f72af6ce0186b70a2fab
Tvan <hothithanhvan.dev@gmail.com>
2025-05-15 22:25:58 +0700

    5. Khóa cuộn khi mở modal,nhấn backdrop không đóng, template.content

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const $$ = document.querySelectorAll.bind(document);
 
 function Modal () {
     this.openModal = function (options = {}) {
-        const {templateId} = options
+        const {templateId, allowBackdropClose = true} = options
         const template = $(`#${templateId}`)
         if(!template) {
             console.error(`${templateId} not found`);
             return;
         }
+        // cloneNode chỉ copy được nội dung của phần tử, không copy  sự kiện handle của phần tử
         const content = template.content.cloneNode(true)
 
         const backdrop = document.createElement("div");
@@ -39,9 +40,11 @@ function Modal () {
             
         }
 
-        backdrop.onclick =  (e) =>{
-            if (e.target === backdrop) {
-                this.closeModal(backdrop);
+        if(allowBackdropClose) {
+            backdrop.onclick =  (e) =>{
+                if (e.target === backdrop) {
+                    this.closeModal(backdrop);
+                }
             }
         }
 
@@ -50,23 +53,44 @@ function Modal () {
                 this.closeModal(backdrop);
             }
         })
+
+        //Disable scroll
+        document.body.classList.add("no-scroll")
+        return backdrop;
+        // vì return lại backdrop nên có thể sử dụng backdrop ở ngoài, ví dụ sau khi mở modal thì có nhiều sự kiện xảy ra trong modal nữa, như submit form
     };
+
     this.closeModal = function(element) {
         element.classList.remove("show");
         element.ontransitionend = function () {
             element.remove(); 
         }
+        document.body.classList.remove("no-scroll")
     }
+    
+    
 }
 
 const modal = new Modal();
 
 $("#open-modal-1").onclick = function () {
-    modal.openModal({templateId: "modal-1"})
+    modal.openModal({
+        templateId: "modal-1"})
 }
 $("#open-modal-2").onclick = function () {
     modal.openModal({templateId: "modal-2"})
 }
 $("#open-modal-3").onclick = function () {
-    modal.openModal({templateId: "modal-3"})
+    const modalElement = modal.openModal({
+        templateId: "modal-3",
+        allowBackdropClose: false}) // không cho phép đóng modal khi click vào backdrop
+
+    const loginForm = modalElement.querySelector("#login-form")
+    if(loginForm) {
+        loginForm.onsubmit = function (e) {
+        e.preventDefault()
+        const name = $(".nameInput")
+        console.log(name.value);
+    }
+    }
 }
